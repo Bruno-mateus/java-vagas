23e7759662fa17a1325080e9d6e193852da898a5
Bruno-mateus <bruno_205@hotmail.com>
2024-05-25 20:16:00 -0300

    add prometheus and grafana

--- src/main/java/br/com/brunomateus/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/brunomateus/gestao_vagas/security/SecurityConfig.java
@@ -21,10 +21,11 @@ public class SecurityConfig {
     SecurityFilterCandidate securityFilterCandidate;
     //indicamos que vamos definir um OBJ ja gerenciado pelo spring
     
-    private static final String[] SWAGGER_LIST = {
+    private static final String[] PERMIT_ALL_LIST = {
         "swagger-ui/**",
         "/v3/api-docs/**",
-        "/swagger-resource/**"
+        "/swagger-resource/**",
+        "/actuator/**"
 };
 
     @Bean
@@ -37,7 +38,7 @@ public class SecurityConfig {
             .requestMatchers("/company/auth").permitAll()
             .requestMatchers("/candidate/auth").permitAll()
             .requestMatchers("/candidate/profile").permitAll()
-            .requestMatchers(SWAGGER_LIST).permitAll();
+            .requestMatchers(PERMIT_ALL_LIST).permitAll();
             auth.anyRequest().authenticated();
         }
         ).addFilterBefore(securityFilterCandidate, BasicAuthenticationFilter.class)
